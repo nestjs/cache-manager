b641e229820c791fb8d906fdee6dccc56f7004b7
Tran Quang <randytran8800@gmail.com>
2023-07-16 21:51:34 +0700

    feat(cache-manager): allow @CacheTTL at the controller level

--- tests/src/custom-ttl/custom-ttl.controller.ts
+++ tests/src/custom-ttl/custom-ttl.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { CacheInterceptor, CacheTTL } from '../../../lib';
 
 @Controller()
+@CacheTTL(600)
 export class CustomTtlController {
   counter = 0;
   constructor() {}
@@ -10,6 +11,12 @@ export class CustomTtlController {
   @CacheTTL(500)
   @UseInterceptors(CacheInterceptor)
   getNumber() {
+    return this.counter++;
+  }
+
+  @Get('/controller')
+  @UseInterceptors(CacheInterceptor)
+  getNumberWithControllerTTL() {
     return this.counter++;
   }
 }
