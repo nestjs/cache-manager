e4b91fd85e4fd9900cc04ed92495824497942591
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2024-10-18 14:52:21 +0200

    chore: use cache-manager-redis-yet store

--- tests/src/multi-store/multi-store.module.ts
+++ tests/src/multi-store/multi-store.module.ts
@@ -1,24 +1,32 @@
 import { Module } from '@nestjs/common';
-import { redisStore } from 'cache-manager-redis-store';
-import { CacheModule } from '../../../lib';
+import { redisStore } from 'cache-manager-redis-yet';
+import { CacheModule, CacheStore } from '../../../lib';
 import { MultiStoreController } from './multi-store.controller';
 
 @Module({
   imports: [
-    CacheModule.register([
-      {
-        store: 'memory',
-        max: 100,
-        ttl: 50,
+    CacheModule.registerAsync({
+      useFactory: async () => {
+        const store = await redisStore({
+          socket: {
+            host: 'localhost',
+            port: 6379,
+          },
+        });
+
+        return [
+          {
+            store: store as unknown as CacheStore,
+            ttl: 3 * 60000, // 3 minutes (milliseconds)
+          },
+          {
+            store: 'memory',
+            max: 100,
+            ttl: 50,
+          },
+        ];
       },
-      {
-        store: redisStore,
-        host: 'localhost',
-        port: 6379,
-        db: 0,
-        ttl: 50,
-      },
-    ]),
+    }),
   ],
   controllers: [MultiStoreController],
 })
